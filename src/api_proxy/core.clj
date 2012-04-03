36995085204bc86abbd6d2f46c8e1c57fd5c1c25
Kyle Burton <kyle.burton@gmail.com>
2012-04-03 19:14:53 -0400

    serve local content

--- src/api_proxy/core.clj
+++ src/api_proxy/core.clj
@@ -6,6 +6,7 @@
    ring.adapter.jetty))
 
 ;; TODO: serve local content [if found]
+;; TODO: serve index.html file if found
 ;; TODO: forward to remote if no local file found (use clj-http)
 ;; TODO: load the configuration from config.json
 ;; TODO: allow multiple proxies to be configured
@@ -22,21 +23,55 @@
                        :num-requests (java.util.concurrent.atomic.AtomicLong.)
                        }})
 
+(defn document-root []
+  "./")
+
 (defn server-atom []
   (get *server* :jetty))
 
 (defn count-request! []
   (.incrementAndGet (get-in *server* [:stats :num-requests])))
 
-(defn request-handler [request]
-  (def *rr* request)
-  (log/infof "got a request: %s" request)
+(defn find-local-file-path [request]
+  (let [f (java.io.File. (document-root)
+                         (:uri request))]
+    (if (and
+         (.isFile f)
+         (.exists f))
+      (str f)
+      ;; try to find /index.html
+        (let [f (java.io.File. (document-root)
+                               (str (:uri request) "/index.html"))]
+    (if (and
+         (.isFile f)
+         (.exists f))
+      (str f))))))
+
+
+(defn serve-local-file [request f]
   {:stutus 200
    :headers {"Content-Type" "text/html"}
-   :body (format "Request %s satisfied at: %s :: <pre>%s</pre>"
-                 (count-request!)
-                 (java.util.Date.)
-                 (with-out-str (clojure.contrib.pprint/pprint request)))})
+   :body (slurp f)})
+
+(defn proxy-stats [request]
+  {:stutus 200
+   :headers {"Content-Type" "text/html"}
+   :body    (with-out-str (clojure.contrib.pprint/pprint (get *server* :stats)))})
+
+(defn proxy-request [request]
+  {:stutus 404
+   :headers {"Content-Type" "text/html"}
+   :body    "Implement proxying to the remote server / service"})
+
+(defn request-handler [request]
+  (count-request!)
+  (def *rr* request)
+  (let [local-file-path (find-local-file-path request)]
+    (cond
+      local-file-path               (serve-local-file request local-file-path)
+      (= "/proxy" (:uri request))   (proxy-stats request)
+      :else                         (proxy-request request))))
+
 
 (defn restart-server []
   (when-not (nil? @(server-atom))
@@ -54,3 +89,38 @@
 
 
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
